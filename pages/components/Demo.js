426f8e48254aa592054c77b35b21e25e2fab4294
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2023-06-07 12:01:40 +0200

    initial commit

--- pages/components/Demo.js
+++ pages/components/Demo.js
@@ -1,6 +1,8 @@
+import VectorLeft from "../../asset/svg/demo-vector-left-pink.svg";
+import VectorRight from "../../asset/svg/demo-vector-right-pink.svg";
 import {useEffect} from "react";
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import {gsap} from 'gsap';
+import {ScrollTrigger} from 'gsap/dist/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -8,7 +10,28 @@ function Demo() {
 
     useEffect(() => {
 
+        gsap.to("#demo .vector-left", {
+            scrollTrigger: {
+                trigger: "#demo",
+                start: "center bottom",
+                end: "top top",
+                scrub: true
+            },
+            strokeDashoffset: "1640px",
+            ease: "linear"
+        })
 
+        gsap.to("#demo .vector-right", {
+            scrollTrigger: {
+                trigger: "#demo",
+                start: "top-=300px center+=50px",
+                end: "center bottom",
+                scrub: true,
+                markers: true
+            },
+            strokeDashoffset: "0px",
+            ease: "linear"
+        })
 
     }, [])
 
@@ -27,7 +50,9 @@ function Demo() {
 
                     <h2 className="title">Elie, un outil pédagogique pour découvrir le vivant</h2>
 
-                    <p className="sub-title">Plongez-vous dans des ambiances, éveillez vos sens,  identifier des espèces et découvrez leurs interactions. Faites du jeu un outils immersif pour stimuler la curiosité des enfants sur le monde du vivant !</p>
+                    <p className="sub-title">Plongez-vous dans des ambiances, éveillez vos sens, identifier des espèces
+                        et découvrez leurs interactions. Faites du jeu un outils immersif pour stimuler la curiosité des
+                        enfants sur le monde du vivant !</p>
 
                     <a href="#">
                         <p className="button">Préparer votre séjour</p>
@@ -37,9 +62,9 @@ function Demo() {
 
             </div>
 
-            <img src={"demo-vector-left.svg"} alt="Vector icon" className="vector-left"/>
+            <VectorLeft className="vector-left"/>
 
-            <img src={"demo-vector-right.svg"} alt="Vector icon" className="vector-right"/>
+            <VectorRight className="vector-right"/>
 
         </section>
     )
