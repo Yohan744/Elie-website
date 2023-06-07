e18804c4d62922be341bcb7e2a55ed2fa4c437d3
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2023-06-07 23:43:27 +0200

    added smooth scroll and started reservation section

--- pages/components/Reservation.js
+++ pages/components/Reservation.js
@@ -5,6 +5,42 @@ function Reservation() {
 
             <div className="global-wrapper">
 
+                <h3 className="title">Venir à l'exposition Elie en <span>4 étapes</span></h3>
+
+                <div className="state-wrapper">
+
+                    <div className="state is-active" id="0">
+                        <div className="state-number">1</div>
+                        <p>Réserver votre créneau</p>
+                    </div>
+
+                    <div className="line"></div>
+
+                    <div className="state" id="1">
+                        <div className="state-number">2</div>
+                        <p>Renseigner votre profil de groupe</p>
+                    </div>
+
+                    <div className="line"></div>
+
+                    <div className="state" id="2">
+                        <div className="state-number">3</div>
+                        <p>Créer les groupes d'élèves</p>
+                    </div>
+
+                    <div className="line"></div>
+
+                    <div className="state" id="3">
+                        <div className="state-number">4</div>
+                        <p>Récapitulatif de votre réservation</p>
+                    </div>
+
+                </div>
+
+                <div className="container">
+
+                </div>
+
             </div>
 
 
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,42 @@ import Gallery from "./components/Gallery";
 import Reservation from "./components/Reservation";
 import Advice from "./components/Advice";
 import Preparation from "./components/Preparation";
+import {useEffect} from "react";
+import Lenis from '@studio-freight/lenis'
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
+
+    useEffect(() => {
+
+        const lenis = new Lenis({
+            duration: 1.25,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+            direction: 'vertical', // vertical, horizontal
+            gestureDirection: 'vertical', // vertical, horizontal, both
+            smooth: true,
+            mouseMultiplier: 1,
+            smoothTouch: false,
+            touchMultiplier: 1.75,
+            infinite: false,
+        })
+        function raf(time) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+        requestAnimationFrame(raf)
+
+        lenis.on('scroll', ScrollTrigger.update)
+
+        gsap.ticker.add((time)=>{
+            lenis.raf(time * 1000)
+        })
+
+    }, [])
+
     return (
         <main>
             <Masthead/>
